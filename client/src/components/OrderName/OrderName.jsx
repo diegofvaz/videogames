4b00c0bf70c2b605926a726961457f525372ed5e
diegofvaz <diegofvaz@gmail.com>
2022-10-24 17:45:23 -0300

    orderName button

--- client/src/components/OrderName/OrderName.jsx
+++ client/src/components/OrderName/OrderName.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { orderName } from '../../redux/actions'
+import style from '../OrderName/OrderName.module.css'
 
 
 const OrderName = () => {
@@ -20,9 +21,9 @@ const OrderName = () => {
     }
 
     return (
-        <div>Order
-            <select name='select' onChange={e=>{handleOrderName(e)}} defaultValue="default">
-                <option value='default' disabled='disabled'>Select</option>
+        <div className={style.content}>
+            <select className={style.select} name='select' onChange={e=>{handleOrderName(e)}} defaultValue="default">
+                <option value='default' disabled='disabled'>Order</option>
                 <option value='all'>All Games</option>
                 <option value='asc'>A - Z</option>  
                 <option value='desc'>Z - A</option>                     
