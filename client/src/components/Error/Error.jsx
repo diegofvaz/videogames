921d87758232f07004316e28098a4275819f88bd
diegofvaz <diegofvaz@gmail.com>
2022-10-25 18:56:21 -0300

    error style

--- client/src/components/Error/Error.jsx
+++ client/src/components/Error/Error.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import style from '../Error/Error.module.css'
 
 
 const Error = () => {
     
     return (
-        <div>
+        <div className={style.content}>
             <h1>Page not found</h1>
             <div>
-                <Link to='/home'><button>Back Home</button></Link>
+                <Link to='/home'><button className={style.button}>Back Home</button></Link>
             </div>
         </div>
     )
