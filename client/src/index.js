5a1fe5d2c94ea2da31bc65c15dd865a703b896a1
diegofvaz <diegofvaz@gmail.com>
2023-01-06 18:17:15 -0300

    import axios

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store/index';
 import { BrowserRouter } from 'react-router-dom'
-
+import axios from "axios";
 
 ReactDOM.render(
   <React.StrictMode>
